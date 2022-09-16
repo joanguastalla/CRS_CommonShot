cc4adf41808983a0b543487f6f0177515a7574f3
Joan Guastalla Arruda <j192806@dac.unicamp.br>
2022-09-16 15:30:38 -0300

    Debugging Lapack usage

--- cubicinterpol.c
+++ cubicinterpol.c
@@ -29,24 +29,23 @@ void printer(int size1,int size2,double* a){
 
 double cubicinterpol(int len,double dt,double* s,double t){
     int ind,n = N,info,nrhs=1;
-    double alpha[2];
+    double rest=0;
+	double alpha[2];
     double A[N][N];
-    double b[N];
+    double b[N],c[N];
+	static double r[N];
     int ipiv[N];
-	printf("Actual value of t is %f\n",t);
-	t=fabs((sgn(t)+1)*t/2);
-    t=min(t,(len-1)*dt);
-	t=max(t,dt);
-	printf("Changed value of t:%f\n",t);
+	//t=fabs((sgn(t)+1)*t/2);
 	ind=t/dt;
-	printf("Used index:%d\n",ind);
     if((double) ind==t/dt){
 		return s[ind];
     } 
     else{
-		ind=min(ind,len-3);
-       	alpha[0]=(s[ind+1]-s[ind-1])/2/dt;
-       	alpha[1]=(s[ind+2]-s[ind])/2/dt; 
+		ind=min(ind,len-2);
+		alpha[0]=(ind>0)?(s[ind+1]-s[ind-1])/(2*dt):(s[ind+1]-s[ind])/dt;
+		alpha[1]=(ind<(len-2))?(s[ind+2]-s[ind])/(2*dt):(s[ind+1]-s[ind])/dt; 
+		 
+
        	for(int ii=0;ii<2;ii++){
             for(int jj=3;jj>=0;jj--){
                 A[ii][3-jj]=pow((ind+ii)*dt,jj);
@@ -58,47 +57,46 @@ double cubicinterpol(int len,double dt,double* s,double t){
             }   
         }
         
-        printf("Matrix A\n");
-        printer(N,N,&A[0][0]);
-        printf("Matrix A after tranposition\n");
         transpose_square(N,A);
-        printer(N,N,&A[0][0]);
         b[0]=s[ind];
         b[1]=s[ind+1];
         b[2]=alpha[0];
         b[3]=alpha[1];
-        printf("Elementos do vetor b\n");
-        printer(1,N,b);
+		c=b;
 		LAPACK_dgesv(&n,&nrhs,&A[0][0],&n,ipiv,b,&n,&info);
-    	printf("Information about LAPACK:%d\n",info);
-    	printf("Value of x solution Ax:b, given by lapack:\n");
-    	printer(1,N,b);
+		for(int ii=0;ii<N;ii++){
+			for(int jj=0;jj<N;jj++){
+				r[ii]+=A[ii][jj]*b[jj];
+			}
+			rest+=pow((r[ii] - c[ii]),2);
+		}
+		printf("%f\n",sqrt(rest));
     	return (b[0]*pow(t,3) + b[1]*pow(t,2)+b[2]*t + b[3]);
-    }
+	}
+		
 }
 
 void wfunc(char* filename,int len,double* x,double* y){
-	char* path="./Fig/";
-	char* filepath=strcat(path,filename);
-	FILE* outfile=fopen(filepath,"w");
+	char filepath[100]="./Fig/";
+	FILE* outfile=fopen(strcat(filepath,filename),"w");
 	for(int ii=0;ii<len;ii++){
-		fprintf(outfile,"%f\t %f\n",x[ii],y[ii]);
+		fprintf(outfile,"%f %f\n",x[ii],y[ii]);
 	}
-
 }
 
 int main(){
     int len=5;
-	int len_t=401;
-	double inc_t=0.01,dt=1;
-    double t[401];
-	double cubic[401];
+	const int len_t=401;
+	double inc_t=0.01,dt=1.0;
+    double t[len_t];
+	double cubic[len_t];
     double s[]={2.8,3,1,3,4};
+	double* p=s;
 	char* cubic_data="cubic_data.txt";
 	for(int ii=0;ii<len_t;ii++){
 		t[ii]=ii*inc_t;
-		cubic[ii]=cubicinterpol(len,dt,s,t[ii]);
+		cubic[ii]=cubicinterpol(len,dt,p,t[ii]);
 	}
-	//wfunc(cubic_data,len_t,t,cubic);
+	wfunc(cubic_data,len_t,t,cubic);
     return 0;
 }
--- sgn.c
+++ sgn.c
@@ -5,6 +5,5 @@ int sgn(double x){
 	int signal;
 	int xx=(x>0)?ceil(x):floor(x);
 	signal=(xx!=0)?abs(xx)/xx:0;
-	printf("In signal function typecast to int gives: %d\n",signal);
 	return signal;
 }
